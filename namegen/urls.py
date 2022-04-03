dc6d176d10f9e5b443f67577ac5f7c092eef92a0
harisw <hari14@mhs.if.its.ac.id>
2022-04-04 02:26:53 +0900

    Done returning good results but still confusing database names

--- namegen/urls.py
+++ namegen/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='namegen_index')
+    path('', views.index, name='namegen_index'),
+    path('api/getnames/', views.getNames, name='namegen_getNames')
 ]
--- namegen/views.py
+++ namegen/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Country, NamesDB
 
 # Create your views here.
 def index(request):
     countries = Country.objects.filter(has_names=True)
     return render(request, 'home.html', {'countries': countries})
+
+def getNames(request):
+    _country = request.GET.get('country', None)
+    _gender = request.GET.get('gender')
+    _keyword = request.GET.get('keyword', '')
+    
+    # if len(_keyword )== 1:
+    #     query_keyword = "'"+_keyword+"%'"
+    # elif len(_keyword) > 1:
+    #     query_keyword = '%'+_keyword
+    
+
+    # if len(_keyword) > 0:
+    #     namesResult = NamesDB.objects.filter(country_id=_country, gender=_gender,
+    #         given_name__icontains=query_keyword)
+    # else:
+    namesResult = NamesDB.objects.filter(country_id=_country, gender=_gender)
+
+    namesResult = namesResult[:10].values()
+    return JsonResponse({'names': list(namesResult)})
